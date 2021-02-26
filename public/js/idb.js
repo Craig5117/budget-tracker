efc27e6677819927863a1526a4c4573678bbc43b
Craig5117 <craig.bennett5117@gmail.com>
2021-02-26 18:15:06 -0500

    set up basic functions in idb.js

---
+++ public/js/idb.js
@@ -0,0 +1,32 @@
+// initialize database store
+let db;
+
+// create the budget_tracker db on indexedDB
+const request = indexedDB.open('budget_tracker', 1);
+
+// on db change store the db changes in budget_tracker db and increment the version number
+request.onupgradeneeded = function(e) {
+    const db = e.target.result;
+    db.createObjectStore('budget_tracker', { autoIncrement: true });
+};
+
+// upon successful db creation, save current db info to the global db variable
+request.onsuccess = function(e) {
+    db = e.target.result;
+    if (navigator.onLine) {
+        // uploadBudgetData();
+    }
+};
+
+// upon failed db creation, log the error
+request.onerror = function(e) {
+    console.log(e.target.errorCode);
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_txn'], 'readwrite');
+
+    const txnObjectStore = transaction.txnObjectStore('new_txn');
+
+    txnObjectStore.add(record);
+}
